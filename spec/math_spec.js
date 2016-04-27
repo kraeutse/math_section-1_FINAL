1f7baad4a5822e2277ab0b86f608054aa8103995
kraeutse <kraeutse@dukes.jmu.edu>
2016-04-27 09:19:03 -0400

    Added files via upload

---
+++ spec/math_spec.js
@@ -0,0 +1,21 @@
+var math = require('../math');
+
+describe("A simple program that does arithmetic", function () {
+	it ("can add two numbers", function() {
+	expect(math.add(3,5)).toBe(8);
+	});
+});
+
+	it("can subtract two numbers", function() {
+		expect(math.subtract(3,5). toBe(-2);		
+	});
+});
+	it ("can multiply two numbers", function() {
+		expect(math.multiply(3,5).toBe(15);
+	});
+});
+	it ("can divide two number". function()
+		expect(math.divide(6,3). toBe (2)
+		expect;
+	});
+});
